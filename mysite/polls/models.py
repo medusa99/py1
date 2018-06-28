3758f5516c243377f56c6e0652133390ed9d6275
medusa99 <tjamesonedm@gmail.com>
2018-06-28 10:01:12 -0400

    Populated database via shell and site, created superuser, and registered model in polls/admin.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-
+#rember to exit out of shell exit() and import modules again so that when Question.objects.all() is called in the shell you see the question return text.
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=100)
     pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.choice_text
     
