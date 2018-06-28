de64fa4ee85232369d6645cc422ef2d422f77fa3
medusa99 <tjamesonedm@gmail.com>
2018-06-28 12:11:48 -0400

    import models into view and added new url paths

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Page of Polls App")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = " , ".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
