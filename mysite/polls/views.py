9921b2802d0e2a0cde6a0158498c38a0dd79a171
medusa99 <tjamesonedm@gmail.com>
2018-06-28 17:19:53 -0400

    Diplay the question text and choice text, and layout- exlucde, include, blocks

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 # Create your views here.
 
@@ -13,7 +14,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
