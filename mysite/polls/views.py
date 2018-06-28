6390ae894723bd4dcbed43e9c5e0a9fa8c4b1437
medusa99 <tjamesonedm@gmail.com>
2018-06-28 14:00:26 -0400

    rendering pages by connecting the view and template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Question
 # Create your views here.
+
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = " , ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+   
+    context =  {
+        'latest_questions':latest_questions
+        }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
